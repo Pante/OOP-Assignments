d8972b1b137e15a4c96e3842037684a1294f432d
Pante <matthiasngeozhongsim@yahoo.com>
2016-10-21 11:52:49 +0800

    Added Q4, Q5 and Q6

---
+++ Week 1/Week1Q4Q5/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application {
+    class Program {
+
+        private static int number;
+        private const string prompt = "Enter a number between 1 and 100 (or -1 to exit)";
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Welcome to the number guessing game");
+            Random random = new Random();
+
+            while (true) {
+                Console.WriteLine(prompt);
+                number = random.Next(1, 101);
+
+                handleGuesses();
+
+                Console.WriteLine("Play again (Y : N) ?: ");
+
+                if (Console.ReadLine().Equals("N", StringComparison.InvariantCultureIgnoreCase)) {
+                    break;
+                }
+            }
+
+        }
+
+        private static void handleGuesses() {
+            while (true) {
+                try {
+                    int guess = int.Parse(Console.ReadLine());
+
+                    if (guess == -1) {
+                        break;
+
+                    } else if (guess > number) {
+                        Console.WriteLine("Your guess it too high!");
+                        Console.WriteLine("Guess again, " + prompt);
+
+                    } else if (guess < number) {
+                        Console.WriteLine("Your guess it too low!");
+                        Console.WriteLine("Guess again, " + prompt);
+
+                    } else {
+                        Console.WriteLine("Bingo! The correct is " + number);
+                        break;
+                    }
+
+                } catch (FormatException e) {
+                    Console.WriteLine("Invalid input, " + prompt);
+                }
+            }
+        }
+
+    }
+}
---
+++ Week 1/Week1Q6/Q6/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6 {
+    class Program {
+        static void Main(string[] args) {
+            new Controller(new Menu(new List<string> {"Cindy", "Kelvin", "Sean", "Lucy", "Sarah", "Melvin"})).handle();
+        }
+    }
+
+    class Controller {
+
+        private Menu menu;
+
+        public Controller(Menu menu) {
+            this.menu = menu;
+        }
+
+
+        public void handle() {
+            while (true) {
+                displayMenu();
+
+                string choice;
+                if ((choice = Console.ReadLine()).Equals("1")) {
+                    menu.addStudent();
+
+                } else if (choice.Equals("2")) {
+                    menu.displayAll();
+
+                } else if (choice.Equals("3")) {
+                    menu.removeStudent();
+
+                } else if (choice.Equals("0")) {
+                    break;
+
+                } else {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+
+        private void displayMenu() {
+            Console.WriteLine("1. Add a new student to the list\n" +
+                            "2 .Display all students in the list\n" + 
+                            "3. Remove an existing student from the list\n" + 
+                            "0. Quit this program"
+                            );
+
+        }
+
+    }
+
+    class Menu {
+
+        private List<string> students;
+
+
+        public Menu(List<string> students) {
+            this.students = students;
+        }
+
+        public void addStudent() {
+            Console.WriteLine("Enter the name of the student to add to the list");
+            string name;
+            students.Add((name = Console.ReadLine()));
+            Console.WriteLine(name + " is added to the list");
+        }
+
+        public void displayAll() {
+            Console.WriteLine("No.  Name. ");
+            foreach (string student in students) {
+                Console.WriteLine(students.IndexOf(student) + "    " + student);
+            }
+        }
+
+        public void removeStudent() {
+            Console.WriteLine("Enter the name of the studet to be removed from the list");
+            string name;
+            students.Remove(name = Console.ReadLine());
+            Console.WriteLine(name + " has been removed from the list");
+        }
+
+    }
+}
