07a13a63f7f5d86358357449163783bb61941b67
Joe Kawai <joe@teamkarbon.com>
2016-10-21 11:00:57 +0800

    Uploaded Week1Q2

---
+++ Week 1/Week1Q2/Week1Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Q2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter a whole number: ");
+            int count = Int32.Parse(Console.ReadLine());
+            int answer = 1;
+
+            if(count == 0) {
+                Console.WriteLine("0! = 1");
+            } else {
+                Console.Write(count + "! = ");
+                for(int i = count; i > 1; i--) {
+                    Console.Write(i + " x ");
+                    answer *= i;
+                }
+                Console.Write("1 = " + answer);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
