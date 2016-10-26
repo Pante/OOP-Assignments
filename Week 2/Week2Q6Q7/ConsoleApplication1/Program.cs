5772f1d6a007194c6175b7fb9cb84d9b5868a66b
Pante <matthiasngeozhongsim@yahoo.com>
2016-10-26 10:58:41 +0800

    Added Questions 6, 7 and 8

---
+++ Week 2/Week2Q6Q7/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program {
+    class Program {
+
+        static void Main(string[] args) {
+            Rectangle outerRectangle = new Rectangle();
+            outerRectangle.length = 20;
+            outerRectangle.width = 14;
+
+            Rectangle innerRectangle = new Rectangle();
+            innerRectangle.length = 15;
+            innerRectangle.width = 8;
+
+            Console.WriteLine("Area of shaded region in rectangle = " + (getArea(outerRectangle) - getArea(innerRectangle)));
+
+            outerRectangle.length = 25;
+            outerRectangle.width = 18;
+
+            Circle circle = new Circle();
+            circle.radius = 5;
+
+            Console.WriteLine("Area of shaded region in rectangle = " + (getArea(outerRectangle) - getArea(circle)));
+        }
+
+        private static double getArea(Rectangle rectangle) {
+            return rectangle.length * rectangle.width;
+        }
+
+        private static double getArea(Circle circle) {
+            return circle.radius * circle.radius * Math.PI;
+        }
+    }
+
+    struct Rectangle {
+        public double length;
+        public double width;
+    }
+
+    struct Circle {
+        public double radius;
+    }
+}
---
+++ Week 2/Week2Q8/Week2Q8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Q8 {
+    class Program {
+        static void Main(string[] args) {
+            printTimesTable(8);
+            printTimesTable(11);
+        }
+
+        static void printTimesTable(int number, int times = 12) {
+            for (int i = 0; i <= times; i++) {
+                Console.WriteLine(i + " x " + number + " = " + (i * number));
+            }
+        }
+    }
+}
