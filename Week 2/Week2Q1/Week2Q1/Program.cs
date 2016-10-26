b15b505d47259b0b97d1614093dba4f1705dc490
Joe Kawai <joe@teamkarbon.com>
2016-10-26 11:13:50 +0800

    Completed Week2Q1

--- Week 2/Week2Q1/Week2Q1/Program.cs
+++ Week 2/Week2Q1/Week2Q1/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Week2Q1 {
     class Program {
         static void Main(string[] args) {
+            Console.Write("Enter a number : ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.Write("The result of adding one to " + num +" is " + addOne(num));
+        }
+
+        static int addOne(int number) {
+            return number++;
         }
     }
 }
