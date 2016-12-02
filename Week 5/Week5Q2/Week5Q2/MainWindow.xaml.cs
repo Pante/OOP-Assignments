4499bf182f0a139b6552f3932b7c22f2d1f15d5d
Joe Kawai <joe@teamkarbon.com>
2016-12-02 10:24:58 +0800

    Completed Week5

---
+++ Week 5/Week5Q2/Week5Q2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week5Q2 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private int onUpdate() {
+
+            if(textBox.Text == "")
+                return 0;
+
+            int count = 1;
+
+            for(int i = 1; i <= Int32.Parse(textBox.Text); i++)
+                count *= i;
+
+            label.Content = count;
+            return count;
+        }
+
+        private double calBMI(double weight, double height) {
+            return Math.Round((weight / (height * height)), 2);
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e) {
+            onUpdate();
+        }
+    }
+}
---
+++ Week 5/Week5Q4/Week5Q4/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week5Q4 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        int number;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            Random rnd = new Random();
+            number = rnd.Next(1, 101);
+        }
+
+        private int onUpdate() {
+            if(textBox.Text == "" || textBox.Text == "-")
+                return 0;
+
+            if(Int32.Parse(textBox.Text) == -1) {
+                Application.Current.Shutdown();
+            } else if(Int32.Parse(textBox.Text) > number) {
+                label.Content = "Your guess is too high!";
+            } else if(Int32.Parse(textBox.Text) < number) {
+                label.Content = "Your guess is too low!";
+            } else {
+                label.Content = "Bingo, the correct answer is " + number;
+            }
+            return 1;
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e) {
+            onUpdate();
+        }
+    }
+}
